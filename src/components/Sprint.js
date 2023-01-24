5c629139f58cc0ea0f179de8c64a52865ad24617
Mauricio Mena <mauricio.mena.arg@gmail.com>
2023-01-24 14:33:22 -0300

    agrego columna por hacer

--- src/components/Sprint.js
+++ src/components/Sprint.js
@@ -7,6 +7,7 @@ import '../components/css/sprint.css'
 function Sprint() {
     const [data , setData ] = useState([]);
     const [backlog , setBacklog ] = useState([]);
+    const [hacer , setHacer ] = useState([]);
     const [totPresupuestado , settotPresupuestado ] = useState(0);
     const [proceso , setProceso ] = useState([]);
     const [testing , setTesting ] = useState([]);
@@ -25,6 +26,7 @@ function Sprint() {
             console.log(data);
                 setData(data.data)
                 setBacklog(data.data.backlog)
+                setHacer(data.data.hacer)
                 setProceso(data.data.proceso)
                 setTesting(data.data.testing)
                 settestingOk(data.data.testingOk)
@@ -43,38 +45,56 @@ function Sprint() {
                 <h1>Sprint {idSprint.id}</h1>
                 { meta.total_puntos && <p> Puntos totales: {meta.total_puntos} </p>  } 
                 { meta.presupuesto && <p> Presupuesto: {Math.round(meta.presupuesto / 60)} Hs. Consumido: {Math.round(meta.consumido/60)} Hs. = {Math.round(( meta.presupuesto - meta.consumido)  / 60)} Hs.</p>  } 
-                
-
-                <div key={'sp'} className='sprint'>                    
+                <div key={'sp'} className='sprint'>
                     <div key={'bkl'} className='backlog'>
                         <h2>Backlog {meta.enBacklog}</h2>
                         { backlog.length > 0 && backlog.map((incidente,index)=>{
-                            return <div className='card' ><p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
-                                
+                            return <div className='card' >  
+                                        <p > {incidente.numero_tarea} </p> 
+                                        <p>{incidente.asunto}</p> 
+                                        <p>Dificultad: {incidente.deno_dificultad}</p>  
+                                        <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> 
+                                        <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p> 
+                                        <i class="fa-duotone fa-light-emergency-on"></i>
+                                    </div>
                         })}
-                    </div>                
+                    </div>   
+
+                    <div key={'hacer'} className='hacer'>
+                        <h2>Por Hacer {meta.enHacer}</h2>
+                        { hacer.length > 0 && hacer.map((incidente,index)=>{
+                            return <div className='card' >  
+                                        <p > {incidente.numero_tarea} </p> 
+                                        <p>{incidente.asunto}</p> 
+                                        <p>Dificultad: {incidente.deno_dificultad}</p>  
+                                        <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> 
+                                        <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p> 
+                                        <i class="fa-duotone fa-light-emergency-on"></i>
+                                    </div>
+                        })}
+                    </div>                   
                     <div className='proceso'>
                         <h2>En Proceso {meta.enProceso}</h2>
                         { proceso.length > 0 && proceso.map(incidente=>{
-                            return <div className='card' ><p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
+                            return <div className='card' > <i class="fa-duotone fa-light-emergency-on"></i>  <p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
                         })}
                     </div>                
                     <div className='testing'>
                         <h2>En Testing {meta.enTesting}</h2>
                         { testing.length > 0 && testing.map(incidente=>{
-                            return <div className='card' ><p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
+                            return <div className='card' > <i class="fa-duotone fa-light-emergency-on"></i> <p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
                         })}
                     </div>                
                     <div className='testingok'>
-                        <h2>Testeado Ok {meta.enTestingOk}</h2>
+                        <h2>Test OK {meta.enTestingOk}</h2>
                         { testingOk.length > 0 && testingOk.map(incidente=>{
-                            return <div className='card' ><p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
+                            return <div className='card' > <i class="fa-duotone fa-light-emergency-on"></i> <p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
                         })}
                     </div>                    
                     <div className='hecho'>
                         <h2>Hecho {meta.hecho} </h2>
                         { hecho.length > 0 && hecho.map(incidente=>{
-                            return <div className='card' ><p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
+                            return <div className='card' > <i class="fa-duotone fa-light-emergency-on"></i> <p > {incidente.numero_tarea} </p> {incidente.asunto} <p>Dificultad: {incidente.deno_dificultad}</p>  <p>Presupuesto: {Math.round(incidente.tiempo_presupuestado / 60)} Hs.</p> <p>Usado: {Math.round(incidente.total_consumido / 60)} Hs.</p></div>
                         })}
                     </div>                                        
                 </div>
