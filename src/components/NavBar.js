40cc984b9d4e8d8b7dfd7b1f3f5271fab2886588
Mauricio Mena <mauricio.mena.arg@gmail.com>
2023-02-28 09:43:00 -0300

    mauri. cambio numero de sprint a 10

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,45 @@
 import './css/navBar.css'
 import Buscar from './Buscar';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function NavBar() {
+  //const  [ respost, setRespost] = useState('');
+   /* prueba post senasa*/
+
+    // const testpost = ()=>{
+    //     var myHeaders = new Headers();
+    //     myHeaders.append("Cookie", "PHPSESSID=ig2ilevf47coccahjtlcf5rovk");
+    //     myHeaders.append("access-token", "token si se usa");
+
+    //     var formdata = new FormData();
+    //     formdata.append("products[0][id_product_code]", "8897");
+    //     formdata.append("products[0][batchId]", "04-21");
+    //     formdata.append("products[0][amount]", "1");
+        
+    //     var requestOptions = {
+    //                         method: 'POST',
+    //                         headers: myHeaders,
+    //                         body: formdata,
+    //                         redirect: 'follow'
+    //                         };
+    //     fetch("https://test.senasa.gov.ar/agrotraza/src/api/Alta_Envio?authUser=ELBOZAL&authPass=ELBOZAL2021&userTaxId=30568664430&sendDate=20230208&senderDepositId=2023&receiverCompanyCuit=30-50065891-2&receiverDepositId=2985", 
+    //     {
+    //         method: 'POST',
+    //         headers: myHeaders,
+    //         body: formdata,
+    //         redirect: 'follow'
+    //         } )
+    //     .then(response => response.text())
+    //     .then(result => {
+    //         console.log(result)
+    //         setRespost(result)
+    //     } )
+    //     .catch(error => {console.log('error', error)});
+
+    // };
+
+    /* prueba post senasa*/
     return ( <>
                 <div className='navBar'>
                     {/* <div>logo</div> */}
@@ -18,7 +55,10 @@ function NavBar() {
                             <Link to='/documentos'> <li>Documentos</li></Link>
                             <Link to='/documentos'> <li>FAQ</li></Link>
                             <Link to='/documentos'> <li>Eventos</li></Link>
-                            <Link to='/sprint/9'>  <li>Sprint</li></Link>
+                            <Link to='/sprint/10'>  <li>Sprint</li></Link>
+                            {/* /prueba post senasa */}
+                            {/* <li onClick={testpost()}>Test post</li> */}
+                            {/* prueba post senasa  */}
                         </ul>
                     </div>
                     {/* <Buscar /> */}
